242741a34f298ce4ecd4a28d6797b0a515c07e3d
Jody Brzovski <jody.brzovski@gmail.com>
2021-01-22 15:15:33 -0500

    working on delivery function

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ let pageSize = 5;  //hold the number of objects we want up on the page
 
 function queryData(zip, radius, status)
 {
-    radius = radius / 0.0022046;
+    radius = parseInt(radius / 0.0022046);
     
     const settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" + zip + ",radius=" + radius,  // + ",limit=50"
+        "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" + zip + "&radius=" + radius + "&limit=50&offset=0",
         "method": "GET",
 
         beforeSend: function(request) {
@@ -22,18 +22,9 @@ function queryData(zip, radius, status)
             console.log(response);
             deliData = response;
             createPageination();
-            if (status == "dine-in")
-            {
-                populateDataDineIn(1);
-                $("#contentMain").hide();
-                $("#restaurantContainer").show(); 
-            }
-            else
-            {
-                populateDataDelivery(1);
-                $("#contentMain").hide();
-                $("#restaurantContainer").show(); 
-            }
+            populateData(1);
+            $("#contentMain").hide();
+            $("#restaurantContainer").show(); 
         })
         .catch(function(response)
         {
@@ -46,7 +37,7 @@ function createPageination()
 {
     var pagination = $("#pagination");
 
-    var length = Math.ceil(deliData.businesses.length / pageSize);
+    var length = Math.ceil(deliData.total / pageSize);
 
     for (i=0; i < length; i++)
     {
@@ -58,70 +49,12 @@ function createPageination()
     {
         var element = event.target;
         var pageNumber = parseInt($(element).attr("data-index"));
-        // populateData(pageNumber);
+        populateData(pageNumber);
     });
 
 }
 
-function populateDataDineIn(page)  //3
-{
-
-    var content = $("#restaurantContainer");
-    content.empty();
-    var pageIndex = (page * pageSize) - pageSize;  //10
-    var pageOffset = (page * pageSize);  //15
-
-    for (i = pageIndex; i < pageOffset; i++)  //while 1=10; i<15
-    {
-        if (i < deliData.businesses.length)
-        {
-            //make the elements to display the restaurants and append to the parent div
-            var div = $("<div>").addClass("grid-x grid-margin-x small-up-2 medium-up-3");
-            var card = $("<div>").addClass("product-card");
-
-            var imgDiv = $("<div>").addClass("product-card-thumbnail");
-            var imgLink = $("<a>").attr("href", deliData.businesses[i].url).attr("target", "_blank");
-            var image = $("<img>").attr("src", deliData.businesses[i].image_url).addClass("product-card-thumbnail-image");
-            var name = $("<h6>").text(deliData.businesses[i].alias);
-            var phone = $("<p>").html(deliData.businesses[i].phone);       
-            var alias = $("<p>").html(deliData.businesses[i].categories[0].alias);
-            var price = $("<h6>").text("Price " + deliData.businesses[i].price);
-            var stars = $("<h6>").text("Stars " + deliData.businesses[i].rating);
-            var mapDiv = $("<div>").addClass("product-card-thumbnail").attr("id", "googleMap" + i);
-
-            imgLink.append(image);
-            imgDiv.append(imgLink);
-            card.append(imgDiv);
-            card.append(name);
-            card.append(phone);
-            card.append(alias);
-            card.append(price);
-            card.append(stars);
-            card.append(mapDiv);
-
-            div.append(card);
-            content.append(div);
-
-            const coor = { lat: deliData.businesses[i].coordinates.latitude, lng: deliData.businesses[i].coordinates.longitude};
-            const map = new google.maps.Map(document.getElementById("googleMap" + i), {
-                zoom: 12,
-                center: coor,
-              });
-              // The marker, positioned at restaurant coordinates
-              const marker = new google.maps.Marker({
-                position: coor,
-                map: map,
-              });
-
-        }
-
-        $("#mapLink").html("Click to view all");
-        $("#mapLink").show();
-    }
-
-}
-
-function populateDataDelivery(page)  //3
+function populateData(page)  //3
 {
 
     var content = $("#restaurantContainer");
